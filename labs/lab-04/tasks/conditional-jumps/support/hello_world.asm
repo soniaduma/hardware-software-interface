24d3983ec12f975ae52d0c0f634fd3bc7b482bfd
Sonia Duma <nicoladuma31@gmail.com>
2026-04-02 12:16:10 +0300

    lab4 wip

--- labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
+++ labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
@@ -4,6 +4,7 @@
 
 section .data
     myString: db "Hello, World!", 0
+    goodbyeString: db "Goodbye, World!", 0
     N: dq 6                         ; N = 6
 
 section .text
@@ -20,17 +21,22 @@ main:
     mov rax, 2
     mov r8, 1
     cmp rax, r8
-    je print                        ; TODO1: rax > r8?
+    jg print                        ; TODO1: rax > r8?
     xor rax, rax
 
     leave
     ret
 
 print:
-    PRINTF64 `%s\n\x0`, myString
-                                    ; TODO2.2: print "Hello, World!" N times
-                                    ; TODO2.1: print "Goodbye, World!"
-    xor rax, rax
+    bucla:
+      push rcx                     ; salvezi rcx pe stivă (PRINTF64 îl poate strica)
+      PRINTF64 `%s\n\x0`, myString ; printează "Hello, World!"
+      pop rcx                      ; restaurezi rcx
+      dec rcx                      ; rcx = rcx - 1
+      jnz bucla                    ; dacă rcx != 0, sari înapoi la bucla
 
-    leave
-    ret
+      PRINTF64 `%s\n\x0`, goodbyeString  ; printează"Goodbye, World!"
+
+      xor rax, rax
+      leave
+      ret
